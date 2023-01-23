6042f70424c6be83073d910ac680c8bf8bc3c94c
SergeyB <sergey.b@prmnd.com>
2023-01-23 17:03:40 +0300

    add localstorage

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -2,12 +2,25 @@ import {Title} from "./components/Title";
 import './Todo.css'
 import {AddRecordForm} from "./components/AddRecordForm";
 import {PowerNameTable} from "./components/PowerNameTable";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 export const Todo = () => {
     const [tableContent, setTableContent] = useState([])
     const [tableHeaders] = useState(['№', 'Дело', 'Описание', 'Статус'])
 
+    useEffect(() => {
+        const tt = JSON.parse(localStorage.getItem('tableComponent'))
+        if (tt) {
+            setTableContent(tt)
+        }
+    }, [])
+
+    useEffect(() => {
+        setTimeout(() => {
+            const tt = JSON.parse(localStorage.getItem('tableComponent'))
+            localStorage.setItem('tableComponent', JSON.stringify(tableContent))
+        }, 0)
+    }, [tableContent])
     const deleteRow = (rowId) => setTableContent(tableContent.filter((element) => element.id !== rowId));
 
 
